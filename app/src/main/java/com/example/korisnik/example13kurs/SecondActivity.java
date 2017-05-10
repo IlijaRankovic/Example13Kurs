ba2dcb9a3b4a00ddd30d1eb70997428241c05d0d
IlijaRankovic <ilijaran@gmail.com>
2017-05-10 23:47:43 +0200

    Pozvana klasa ThirdActivity iz klase SecondActivity fixes #7

--- app/src/main/java/com/example/korisnik/example13kurs/SecondActivity.java
+++ app/src/main/java/com/example/korisnik/example13kurs/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.korisnik.example13kurs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 /**
@@ -64,5 +66,11 @@ public class SecondActivity extends Activity {
 
         Toast toast = Toast.makeText(getBaseContext(), "SecondActivity.onDestroy", Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void btnStartActivityClicked(View view){
+
+        Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
